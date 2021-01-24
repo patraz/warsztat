ffb7b9df96c484cbfede9a862f0403760508c1f8
Patryk <patrykprause@gmail.com>
2021-01-24 19:11:56 +0100

    g

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ let zalogowano = false;
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: true,
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
     }
 });
 
