10ed847b59a9ec5e03fb7c80bc6a14de2145bc76
Patryk <patrykprause@gmail.com>
2021-01-24 18:03:19 +0100

    changing bcrypt

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const knex = require('knex');
 const path = require('path')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { env } = require('process');
  
 
